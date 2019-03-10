a4127a1f633414c31de3954309086e9a16f31101
edugarcia98 <edugarcia758@gmail.com>
2019-03-10 16:15:16 -0300

    Criando alguns modelos e finalizando cadastro de Curso

--- admFacens/admin.py
+++ admFacens/admin.py
@@ -1,3 +1,64 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+
+#Classes:
+
+class SemestreAdmin(admin.ModelAdmin):
+    fields = ['number', 'year']
+    list_display = ('number', 'year')
+    list_filter = ['year']
+
+class SemestreInline(admin.TabularInline):
+    model = Curso.semestres.through
+    extra = 1
+    verbose_name = "Semestre"
+    verbose_name_plural = "Semestres"
+    classes = ['collapse']
+    
+class ObjetivoAdmin(admin.ModelAdmin):
+    fields = ['title', 'description']
+    list_display = ('title', 'description')
+
+class ObjetivoInline(admin.TabularInline):
+    model = Curso.objetivos.through
+    extra = 1
+    verbose_name = "Objetivo"
+    verbose_name_plural = "Objetivos"
+    classes = ['collapse']
+
+class CompetenciaAdmin(admin.ModelAdmin):
+    fields = ['title', 'description']
+    list_display = ('title', 'description')
+
+class CompetenciaInline(admin.TabularInline):
+    model = Curso.competencias.through
+    extra = 1
+    verbose_name = "Competencia"
+    verbose_name_plural = "Competencias"
+    classes = ['collapse']
+
+class HabilidadeAdmin(admin.ModelAdmin):
+    fields = ['title', 'description']
+    list_display = ('title', 'description')
+
+class HabilidadeInline(admin.TabularInline):
+    model = Curso.habilidades.through
+    extra = 1
+    verbose_name = "Habilidade"
+    verbose_name_plural = "Habilidades"
+    classes = ['collapse']
+
+class CursoAdmin(admin.ModelAdmin):
+    fields = ['title', 'description']
+    inlines = [SemestreInline, ObjetivoInline, CompetenciaInline, HabilidadeInline]
+    list_display = ['title', 'description']
+
+#Registros do Admin
+
+admin.site.register(Semestre, SemestreAdmin)
+admin.site.register(Objetivo, ObjetivoAdmin)
+admin.site.register(Competencia, CompetenciaAdmin)
+admin.site.register(Habilidade, HabilidadeAdmin)
+admin.site.register(Curso, CursoAdmin)
--- admFacens/models.py
+++ admFacens/models.py
@@ -1,3 +1,58 @@
 from django.db import models
+from django.core.validators import MaxValueValidator, MinValueValidator
 
 # Create your models here.
+
+class Semestre(models.Model):
+    number = models.IntegerField(validators = [MinValueValidator(1), MaxValueValidator(10)], verbose_name = "Semestre")
+    year = models.IntegerField(validators = [MinValueValidator(1900)], verbose_name = "Ano")
+
+    def __str__(self):
+        return str(self.number) + "/" + str(self.year)
+
+    class Meta:
+        ordering = ('number',)
+
+class Objetivo(models.Model):
+    title = models.CharField(max_length = 50, verbose_name = "Objetivo")
+    description = models.CharField(max_length = 1000, verbose_name = "Descricao")
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        ordering = ('title',)
+
+class Competencia(models.Model):
+    title = models.CharField(max_length = 50, verbose_name = "Competencia")
+    description = models.CharField(max_length = 1000, verbose_name = "Descricao")
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        ordering = ('title',)
+
+class Habilidade(models.Model):
+    title = models.CharField(max_length = 50, verbose_name = "Habilidade")
+    description = models.CharField(max_length = 1000, verbose_name = "Descricao")
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        ordering = ('title',)
+
+class Curso(models.Model):
+    title = models.CharField(max_length = 50, verbose_name = "Curso")
+    description = models.CharField(max_length = 1000, verbose_name = "Descricao")
+    semestres = models.ManyToManyField(Semestre)
+    objetivos = models.ManyToManyField(Objetivo)
+    competencias = models.ManyToManyField(Competencia)
+    habilidades = models.ManyToManyField(Habilidade)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        ordering = ('title',)
