5166ad6a8e01ef1c2972bf044b298aa493240332
edugarcia98 <edugarcia758@gmail.com>
2019-03-17 16:18:57 -0300

    Criando modelos

--- admFacens/admin.py
+++ admFacens/admin.py
@@ -5,18 +5,6 @@ from .models import *
 
 #Classes:
 
-class SemestreAdmin(admin.ModelAdmin):
-    fields = ['number', 'year']
-    list_display = ('number', 'year')
-    list_filter = ['year']
-
-class SemestreInline(admin.TabularInline):
-    model = Curso.semestres.through
-    extra = 1
-    verbose_name = "Semestre"
-    verbose_name_plural = "Semestres"
-    classes = ['collapse']
-    
 class ObjetivoAdmin(admin.ModelAdmin):
     fields = ['title', 'description']
     list_display = ('title', 'description')
@@ -50,15 +38,105 @@ class HabilidadeInline(admin.TabularInline):
     verbose_name_plural = "Habilidades"
     classes = ['collapse']
 
+class LivroAdmin(admin.ModelAdmin):
+    fields = ['title', 'autor', 'bibliografia']
+    list_display = ['title', 'autor', 'bibliografia']
+
+class DisciplinaAdmin(admin.ModelAdmin):
+    fields = ['title', 'tipo', 'creditos']
+    list_display = ['title', 'tipo', 'creditos', 'horas_aula']
+
+class DisciplinaInline(admin.TabularInline):
+    model = Curso.disciplinas.through
+    exclude = ['objetivos', 'competencias', 'habilidades']
+    extra = 1
+    verbose_name = "Disciplina"
+    verbose_name_plural = "Disciplinas"
+    classes = ['collapse']
+
 class CursoAdmin(admin.ModelAdmin):
     fields = ['title', 'description']
-    inlines = [SemestreInline, ObjetivoInline, CompetenciaInline, HabilidadeInline]
+    inlines = [ObjetivoInline, CompetenciaInline, HabilidadeInline, DisciplinaInline]
     list_display = ['title', 'description']
+
+"""
+class CursoDisciplinaObjetivoInline(admin.TabularInline):
+    model = CursoDisciplina.objetivos.through
+    extra = 1
+    verbose_name = "Objetivo"
+    verbose_name_plural = "Objetivos"
+    classes = ['collapse']
+
+class CursoDisciplinaCompetenciaInline(admin.TabularInline):
+    model = CursoDisciplina.competencias.through
+    extra = 1
+    verbose_name = "Competencia"
+    verbose_name_plural = "Competencias"
+    classes = ['collapse']
+
+class CursoDisciplinaHabilidadeInline(admin.TabularInline):
+    model = CursoDisciplina.habilidades.through
+    extra = 1
+    verbose_name = "Habilidade"
+    verbose_name_plural = "Habilidades"
+    classes = ['collapse']
+"""
+
+class TurmaInline(admin.TabularInline):
+    model = CursoDisciplina.turmas.through
+    extra = 1
+    verbose_name="Turma"
+    verbose_name_plural="Turmas"
+    classes = ['collapse']
+
+class AulaInline(admin.TabularInline):
+    model = CursoDisciplinaTurma.aulas.through
+    extra = 1
+    verbose_name="Aula"
+    verbose_name_plural="Aulas"
+    classes = ['collapse']
+
+class CursoDisciplinaLivroInline(admin.TabularInline):
+    model = CursoDisciplina.livros.through
+    extra = 1
+    verbose_name="Livro"
+    verbose_name_plural="Livros"
+    classes = ['collapse']
+
+class CursoDisciplinaTurmaAulaLivroInline(admin.TabularInline):
+    model = CursoDisciplinaTurmaAula.livros.through
+    extra = 1
+    verbose_name="Livro"
+    verbose_name_plural="Livros"
+    classes = ['collapse']
+
+class CursoDisciplinaAdmin(admin.ModelAdmin):
+    fields = ['curso', 'disciplina']
+    #inlines = [CursoDisciplinaObjetivoInline, CursoDisciplinaCompetenciaInline, CursoDisciplinaHabilidadeInline]
+    inlines = [TurmaInline, CursoDisciplinaLivroInline]
+    list_display = ['curso', 'disciplina']
+
+class CursoDisciplinaTurmaAdmin(admin.ModelAdmin):
+    fields = ['cursoDisciplina', 'turma']
+    inlines = [AulaInline]
+    list_display = ['cursoDisciplina', 'turma']
+
+class CursoDisciplinaTurmaAulaAdmin(admin.ModelAdmin):
+    fields = ['cursoDisciplinaTurma', 'aula']
+    inlines = [CursoDisciplinaTurmaAulaLivroInline]
+    list_display = ['cursoDisciplinaTurma', 'aula']
+
 
 #Registros do Admin
 
-admin.site.register(Semestre, SemestreAdmin)
 admin.site.register(Objetivo, ObjetivoAdmin)
 admin.site.register(Competencia, CompetenciaAdmin)
 admin.site.register(Habilidade, HabilidadeAdmin)
 admin.site.register(Curso, CursoAdmin)
+admin.site.register(Disciplina, DisciplinaAdmin)
+admin.site.register(CursoDisciplina, CursoDisciplinaAdmin)
+admin.site.register(CursoDisciplinaTurma, CursoDisciplinaTurmaAdmin)
+admin.site.register(CursoDisciplinaTurmaAula, CursoDisciplinaTurmaAulaAdmin)
+admin.site.register(Livro, LivroAdmin)
+admin.site.register(Turma)
+admin.site.register(Aula)
--- admFacens/models.py
+++ admFacens/models.py
@@ -1,17 +1,8 @@
 from django.db import models
 from django.core.validators import MaxValueValidator, MinValueValidator
+from django.core.exceptions import ValidationError
 
 # Create your models here.
-
-class Semestre(models.Model):
-    number = models.IntegerField(validators = [MinValueValidator(1), MaxValueValidator(10)], verbose_name = "Semestre")
-    year = models.IntegerField(validators = [MinValueValidator(1900)], verbose_name = "Ano")
-
-    def __str__(self):
-        return str(self.number) + "/" + str(self.year)
-
-    class Meta:
-        ordering = ('number',)
 
 class Objetivo(models.Model):
     title = models.CharField(max_length = 50, verbose_name = "Objetivo")
@@ -43,6 +34,159 @@ class Habilidade(models.Model):
     class Meta:
         ordering = ('title',)
 
+class Disciplina(models.Model):
+    DISC_TYPES = (
+        ('T', 'Teorica'),
+        ('P', 'Pratica'),
+    )
+
+    CREDITO_TYPES = (
+        (1, 2),
+        (2, 4),
+        (3, 6),
+        (4, 8),
+    )
+
+    title = models.CharField(max_length=100, verbose_name="Disciplina")
+    tipo = models.CharField(max_length=1, choices=DISC_TYPES, verbose_name="Tipo")
+    creditos = models.IntegerField(choices=CREDITO_TYPES, verbose_name="Creditos")
+
+    def _get_horas_aula(self):
+        return self.creditos * 20
+
+    horas_aula = property(_get_horas_aula)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        ordering = ('title',)
+
+class Turma(models.Model):
+    codigo = models.CharField(max_length=10, verbose_name="Codigo")
+
+    def __str__(self):
+        return self.codigo
+
+    class Meta:
+        ordering = ('codigo',)
+
+class Aula(models.Model):
+    AULA_TYPES = (
+        ('T', 'Teorica'),
+        ('P', 'Pratica'),
+        ('-', 'Nenhum'),
+    )
+
+    semana = models.IntegerField(validators = [MinValueValidator(1)], verbose_name="Semana")
+    data = models.DateField(verbose_name="Data")
+    tipo = models.CharField(max_length=1, choices=AULA_TYPES, verbose_name="Tipo")
+    conteudo = models.CharField(max_length=100, verbose_name="Conteudo")
+
+    def __str__(self):
+        return self.conteudo
+
+    class Meta:
+        ordering = ('conteudo',)
+
+class Livro(models.Model):
+    LIVRO_TYPES = (
+        ('B', 'Basica'),
+        ('C', 'Complementar'),
+    )
+
+    title = models.CharField(max_length=200, verbose_name="Titulo")
+    autor = models.CharField(max_length=500, verbose_name="Autor(es)")
+    bibliografia = models.CharField(max_length=1, choices=LIVRO_TYPES, verbose_name="Bibliografia")
+
+    def __str__(self):
+        return self.title + " - " + self.autor
+
+    class Meta:
+        ordering = ('title',)
+
+class Curso(models.Model):
+    title = models.CharField(max_length=50, verbose_name="Curso")
+    description = models.CharField(max_length=1000, verbose_name="Descricao")
+    objetivos = models.ManyToManyField(Objetivo, through='CursoObjetivo')
+    competencias = models.ManyToManyField(Competencia, through='CursoCompetencia')
+    habilidades = models.ManyToManyField(Habilidade, through='CursoHabilidade')
+    disciplinas = models.ManyToManyField(Disciplina, through='CursoDisciplina')
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        ordering = ('title',)
+
+class CursoObjetivo(models.Model):
+    curso = models.ForeignKey(Curso, on_delete=models.CASCADE)
+    objetivo = models.ForeignKey(Objetivo, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.curso.title + " - " + self.objetivo.title
+
+class CursoCompetencia(models.Model):
+    curso = models.ForeignKey(Curso, on_delete=models.CASCADE)
+    competencia = models.ForeignKey(Competencia, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.curso.title + " - " + self.competencia.title
+
+class CursoHabilidade(models.Model):
+    curso = models.ForeignKey(Curso, on_delete=models.CASCADE)
+    habilidade = models.ForeignKey(Habilidade, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.curso.title + " - " + self.habilidade.title
+
+class CursoDisciplina(models.Model):
+    curso = models.ForeignKey(Curso, on_delete=models.CASCADE)
+    disciplina = models.ForeignKey(Disciplina, on_delete=models.CASCADE)
+    objetivos = models.ManyToManyField(CursoObjetivo, blank=True)
+    competencias = models.ManyToManyField(CursoCompetencia, blank=True)
+    habilidades = models.ManyToManyField(CursoHabilidade, blank=True)
+    turmas = models.ManyToManyField(Turma, through='CursoDisciplinaTurma')
+    livros = models.ManyToManyField(Livro, through='CursoDisciplinaLivro')
+
+    def __str__(self):
+        return self.curso.title + " - " + self.disciplina.title
+
+class CursoDisciplinaTurma(models.Model):
+    cursoDisciplina = models.ForeignKey(CursoDisciplina, on_delete=models.CASCADE)
+    turma = models.ForeignKey(Turma, on_delete=models.CASCADE)
+    aulas = models.ManyToManyField(Aula, through='CursoDisciplinaTurmaAula')
+
+    def __str__(self):
+        return self.turma.codigo
+
+class CursoDisciplinaLivro(models.Model):
+    cursoDisciplina = models.ForeignKey(CursoDisciplina, on_delete=models.CASCADE)
+    livro = models.ForeignKey(Livro, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return str(self.cursoDisciplina) + " - " + str(self.livro)
+
+class CursoDisciplinaTurmaAula(models.Model):
+    cursoDisciplinaTurma = models.ForeignKey(CursoDisciplinaTurma, on_delete=models.CASCADE)
+    aula = models.ForeignKey(Aula, on_delete=models.CASCADE)
+    livros = models.ManyToManyField(CursoDisciplinaLivro)
+
+    def __str__(self):
+        return str(self.cursoDisciplinaTurma) + " - " + str(self.aula)
+
+
+"""
+class Semestre(models.Model):
+    number = models.IntegerField(validators = [MinValueValidator(1), MaxValueValidator(10)], verbose_name = "Semestre")
+    year = models.IntegerField(validators = [MinValueValidator(1900)], verbose_name = "Ano")
+
+    def __str__(self):
+        return str(self.number) + "/" + str(self.year)
+
+    class Meta:
+        ordering = ('number',)
+
 class Curso(models.Model):
     title = models.CharField(max_length = 50, verbose_name = "Curso")
     description = models.CharField(max_length = 1000, verbose_name = "Descricao")
@@ -56,3 +200,4 @@ class Curso(models.Model):
 
     class Meta:
         ordering = ('title',)
+"""
